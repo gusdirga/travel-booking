618fc45af90f8521f06b56f0a143c5de307ccebf
Gus Dirga <gusdirga@gmail.com>
2025-04-30 21:48:36 +0800

    create admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware('auth')->group(function () {
+    Route::get('/admin', [ProfileController::class, 'edit'])->name('profile.edit');
+});
+
 require __DIR__.'/auth.php';
